2bfe861fa15137ad58d2a835006df556c2991b3c
ShawnTrack <shawntrack@gmail.com>
2025-08-17 16:11:29 +0200

    Corrección script movimiento

--- escenas/player.gd
+++ escenas/player.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	var direction = Vector3.ZERO
 
 	# We check for each move input and update the direction accordingly.
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
